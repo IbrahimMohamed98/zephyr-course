2034076cf2a39a3a68e89fe8d990b7c2f3d9125c
Ibrahim Bashar <ibrahim.bashar@seitech-solutions.com>
2026-09-12 15:51:38 +0300

    feat: add custom my_board target for QEMU x86

---
+++ app/boards/my_board/board.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/sys/printk.h>
+
+static int my_board_init(void)
+{
+	printk("Board Initialized\n");
+	return 0;
+}
+
+SYS_INIT(my_board_init, POST_KERNEL, CONFIG_APPLICATION_INIT_PRIORITY);
---
+++ app/boards/my_board/board.cmake
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: Apache-2.0
+
+include(${ZEPHYR_BASE}/boards/qemu/x86/board.cmake)

b87a12be80965f1199572e6d3bbc50f7b9bd8e41
Ibrahim Bashar <ibrahim.bashar@seitech-solutions.com>
2026-09-24 11:40:54 +0300

    Implement LED sensor driver for Home Task 1

---
+++ our-driver-module/drivers/our_driver/our_driver.c
@@ -0,0 +1,70 @@
+#define DT_DRV_COMPAT our_driver
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(our_driver, CONFIG_SENSOR_LOG_LEVEL);
+
+struct our_driver_config {
+	struct gpio_dt_spec led;
+};
+
+static int our_driver_sample_fetch(const struct device *dev, enum sensor_channel chan)
+{
+	ARG_UNUSED(chan);
+	const struct our_driver_config *cfg = dev->config;
+
+	return gpio_pin_set_dt(&cfg->led, 1);
+}
+
+static int our_driver_channel_get(const struct device *dev,
+				  enum sensor_channel chan,
+				  struct sensor_value *val)
+{
+	ARG_UNUSED(chan);
+	ARG_UNUSED(val);
+	const struct our_driver_config *cfg = dev->config;
+
+	return gpio_pin_set_dt(&cfg->led, 0);
+}
+
+static int our_driver_init(const struct device *dev)
+{
+	const struct our_driver_config *cfg = dev->config;
+
+	if (!gpio_is_ready_dt(&cfg->led)) {
+		LOG_ERR("GPIO device %s not ready", cfg->led.port->name);
+		return -ENODEV;
+	}
+
+	int ret = gpio_pin_configure_dt(&cfg->led, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Failed to configure GPIO (%d)", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static const struct sensor_driver_api our_driver_api = {
+	.sample_fetch = our_driver_sample_fetch,
+	.channel_get = our_driver_channel_get,
+};
+
+#define OUR_DRIVER_DEFINE(inst)                                                \
+	static const struct our_driver_config our_driver_config_##inst = {         \
+		.led = GPIO_DT_SPEC_INST_GET(inst, led_gpios),                        \
+	};                                                                         \
+                                                                               \
+	DEVICE_DT_INST_DEFINE(inst,                                                \
+			      our_driver_init,                                         \
+			      NULL,                                                    \
+			      NULL,                                                    \
+			      &our_driver_config_##inst,                               \
+			      POST_KERNEL,                                             \
+			      CONFIG_SENSOR_INIT_PRIORITY,                             \
+			      &our_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(OUR_DRIVER_DEFINE)
---
+++ our-driver-module/include/our_drivers/our_driver.h
@@ -0,0 +1,17 @@
+#ifndef OUR_DRIVERS_OUR_DRIVER_H_
+#define OUR_DRIVERS_OUR_DRIVER_H_
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Public driver header and API definitions */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OUR_DRIVERS_OUR_DRIVER_H_ */
